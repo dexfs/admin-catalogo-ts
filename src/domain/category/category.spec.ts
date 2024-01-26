b6be426c354949e9f466bc107872e35cd1e9557c
Andre Santos <andre@dexdev.com.br>
2024-01-25 22:17:13 -0300

    test: create CategoryID ValueObject and use in Category Entity

--- src/domain/category/category.spec.ts
+++ src/domain/category/category.spec.ts
@@ -1,5 +1,6 @@
 import {describe, expect, test} from "vitest";
 import {Category} from "./category";
+import {CategoryID} from "./categoryID";
 
 describe("Category Entity", () => {
     test("Given a valida params, when call newCategory, then instiate a Category", () =>{
@@ -9,9 +10,13 @@ describe("Category Entity", () => {
             active: true,
         }
 
-        const sut = Category.newCategory(expected.name, expected.description, expected.active)
+        const expectedId = CategoryID.unique()
+
+        const sut = Category.newCategory(expected.name, expected.description, expected.active, expectedId)
         expect(sut).to.be.instanceof(Category)
         expect(sut.id).not.toBeNull();
+        expect(sut.id).toEqual(expectedId);
+        expect(sut.id.equals(expectedId)).toBeTruthy();
         expect(sut.name).not.toBeNull();
         expect(sut.description).not.toBeNull();
         expect(sut.active).not.toBeNull();
