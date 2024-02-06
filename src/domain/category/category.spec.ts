b09529a82c7b6e8a38566b567b83e1a086d5521b
Andre Santos <andre@dexdev.com.br>
2024-02-06 20:37:49 -0300

    test: category validation

--- src/domain/category/category.spec.ts
+++ src/domain/category/category.spec.ts
@@ -28,15 +28,15 @@ describe("Category Entity", () => {
     test('Given an invalid null name, when call new category and validate, then should receive error', () => {
         expect.hasAssertions()
         const expected = {
-            name: "Fi ",
-            description: "A categoria mais assistida",
+            name: null,
+            description: null,
             active: true,
         }
 
         const sut = Category
             .newCategory(
-                expected.name,
-                expected.description,
+                expected.name as any,
+                expected.description as any,
                 expected.active
             )
         try {
@@ -49,4 +49,73 @@ describe("Category Entity", () => {
             errors.forEach((e, index) => expect(e.message).toBe(expectedMessages[index]))
         }
     })
+
+    test('Given an invalid name length less than 3, when call call new category and validate, then should receive error', () => {
+        expect.hasAssertions()
+        const expected = {
+            name: 'Fi ',
+            description: 'A categoria mais assistida',
+            active: true,
+        }
+        const expectedMessages = ["'name' must be between 3 and 255 characters"]
+
+        const actualCategory = Category
+            .newCategory(expected.name, expected.description, expected.active)
+
+        try {
+            actualCategory.validate(new ThrowsValidationHandler())
+        } catch(e) {
+            const errors = e as DomainException[]
+            expect(errors).toHaveLength(1)
+            errors.forEach(e => expect(e).toBeInstanceOf(Error))
+            errors.forEach((e, index) => expect(e.message).toBe(expectedMessages[index]))
+        }
+    })
+
+    test('Given an invalid name length greater than 255, when call call new category and validate, then should receive error', () => {
+        expect.hasAssertions()
+        const expected = {
+            name: `No entanto, não podemos esquecer que o desenvolvimento contínuo de distintas formas de atuação apresenta tendências no sentido de aprovar a manutenção dos paradigmas corporativos.
+                Gostaria de enfatizar que o surgimento do comércio virtual faz parte de um processo de gerenciamento do remanejamento dos quadros funcionais. Ainda assim, existem dúvidas a respeito.`,
+            description: 'A categoria mais assistida',
+            active: true,
+        }
+        const expectedMessages = ["'name' must be between 3 and 255 characters"]
+
+        const actualCategory = Category
+            .newCategory(expected.name, expected.description, expected.active)
+
+        try {
+            actualCategory.validate(new ThrowsValidationHandler())
+        } catch(e) {
+            const errors = e as DomainException[]
+            expect(errors).toHaveLength(1)
+            errors.forEach(e => expect(e).toBeInstanceOf(Error))
+            errors.forEach((e, index) => expect(e.message).toBe(expectedMessages[index]))
+        }
+    })
+
+    test('Given a valid empty description, when call new category, then instantiate a Category', () => {
+        expect.hasAssertions()
+        const expected = {
+            name: 'Filmes',
+            description: 'A categoria mais assistida',
+            active: true,
+        }
+        const actualCategory = Category
+            .newCategory(expected.name, expected.description, expected.active)
+
+        expect(() => actualCategory.validate(new ThrowsValidationHandler())).not.toThrow()
+
+        expect(actualCategory).toBeInstanceOf(Category)
+        expect(actualCategory.id).not.toBeNull();
+        expect(actualCategory.id).not.toBeNull();
+        expect(actualCategory.name).not.toBeNull();
+        expect(actualCategory.description).not.toBeNull();
+        expect(actualCategory.active).not.toBeNull();
+        expect(actualCategory.createdAt).not.toBeNull();
+        expect(actualCategory.updatedAt).not.toBeNull();
+        expect(actualCategory.deletedAt).toBeNull();
+    })
+
 })
