e47ebb50b51deb7f8aa0947f6eb0b959665f3750
Andre Santos <andre@dexdev.com.br>
2024-02-06 22:21:42 -0300

    test: category deactivate

--- src/domain/category/category.spec.ts
+++ src/domain/category/category.spec.ts
@@ -1,10 +1,18 @@
-import {describe, expect, test} from "vitest";
+import {describe, expect, test, vi, beforeEach, afterEach} from "vitest";
 import {Category} from "./category";
 import {CategoryID} from "./categoryID";
 import {ThrowsValidationHandler} from "../validation/handler/throws-validation.handler";
 import {DomainException} from "../exceptions/domain.exception";
 
 describe("Category Entity", () => {
+    beforeEach(() => {
+        vi.useFakeTimers()
+    })
+
+    afterEach(() => {
+        vi.useRealTimers()
+    })
+
     test("Given a valid params, when call newCategory, then Instantiate  a Category", () =>{
         const expected = {
             name: 'Filmes',
@@ -43,8 +51,8 @@ describe("Category Entity", () => {
             sut.validate(new ThrowsValidationHandler())
         } catch (e) {
             const errors = e as DomainException[]
-            const expectedMessages = ["'name' should not be null", "'description' should not be null"]
-            expect(errors).toHaveLength(2)
+            const expectedMessages = ["'name' should not be null"]
+            expect(errors).toHaveLength(1)
             errors.forEach(e => expect(e).toBeInstanceOf(Error))
             errors.forEach((e, index) => expect(e.message).toBe(expectedMessages[index]))
         }
@@ -139,6 +147,40 @@ describe("Category Entity", () => {
         expect(actualCategory.active).toBeFalsy();
         expect(actualCategory.createdAt).not.toBeNull();
         expect(actualCategory.updatedAt).not.toBeNull();
+        expect(actualCategory.deletedAt).not.toBeNull();
+    })
+
+    test('Given a valid active category, when called deactivate, then return Category inactivate', () => {
+        expect.hasAssertions()
+        let date = new Date(2024, 1, 1, 13)
+        vi.setSystemTime(date)
+        const expected = {
+            name: 'Filmes',
+            description: 'A categoria mais assistida',
+            isActive: false,
+        }
+        const aCategory = Category
+            .newCategory(expected.name, expected.description, true)
+
+        expect(() => aCategory.validate(new ThrowsValidationHandler())).not.toThrow()
+
+        const createdAt = aCategory.createdAt;
+        const updatedAt = aCategory.updatedAt
+        expect(aCategory.active).toBeTruthy();
+        expect(aCategory.deletedAt).toBeNull();
+
+        date = new Date(2024, 1, 1, 14)
+        vi.setSystemTime(date)
+        const actualCategory = aCategory.deactivate()
+
+        expect(actualCategory).toBeInstanceOf(Category)
+        expect(aCategory.id).toEqual(actualCategory.id);
+        expect(actualCategory.name).toBe(expected.name);
+        expect(actualCategory.description).toBe(expected.description);
+        expect(actualCategory.active).toBe(expected.isActive);
+        expect(actualCategory.active).toBeFalsy();
+        expect(actualCategory.createdAt).toBe(createdAt);
+        expect(actualCategory.updatedAt > updatedAt).toBeTruthy();
         expect(actualCategory.deletedAt).not.toBeNull();
     })
 
