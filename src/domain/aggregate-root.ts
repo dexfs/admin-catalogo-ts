ddec444a3f34e0458592c4f96f600fbd8038d504
Andre Santos <andre@dexdev.com.br>
2024-03-18 20:42:15 -0300

    chore: lint

--- src/domain/aggregate-root.ts
+++ src/domain/aggregate-root.ts
@@ -2,7 +2,6 @@ import {Identifier} from "./identifier";
 import {Entity} from "./Entity";
 
 export abstract class AggregateRoot<ID extends Identifier> extends Entity<ID> {
-
     constructor(id: ID) {
         super(id);
     }
